d98a92045a6c4714c07c36f0d89aad506fa593a8
lucassouza16 <lucasferreiradesouza16@gmail.com>
2023-06-11 19:37:08 -0300

    Update AppUtil.java

--- android/src/main/java/com/lucassouza/firebase/alarm/firebase_alarm_notification/util/AppUtil.java
+++ android/src/main/java/com/lucassouza/firebase/alarm/firebase_alarm_notification/util/AppUtil.java
@@ -1,14 +1,13 @@
 package com.lucassouza.firebase.alarm.firebase_alarm_notification.util;
 
 import android.app.ActivityManager;
-import android.content.Context;
 import android.os.SystemClock;
 public class AppUtil {
     public static int genUniqueID() {
         return ((int) SystemClock.uptimeMillis() % 99999999);
     }
 
-    public static boolean isAppForeground(Context context) {
+    public static boolean isAppForeground() {
         ActivityManager.RunningAppProcessInfo appProcessInfo = new ActivityManager.RunningAppProcessInfo();
         ActivityManager.getMyMemoryState(appProcessInfo);
         return (appProcessInfo.importance == ActivityManager.RunningAppProcessInfo.IMPORTANCE_FOREGROUND ||
